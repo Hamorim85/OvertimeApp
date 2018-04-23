e04a5d46bc5e98ac9fb4c8b8c53b287319abe248
Hugo Amorim <hugo@startupdesignninja.com>
2018-04-23 11:46:00 -0300

    table created in post, new seed with user

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe "Creation" do
     before do
-      @post = Post.create(date: Date.today, rationale: "Anything")
+      @post = Post.create(date: Date.today, rationale: "Anything", user_id: 1)
     end
     it "can be created" do
       expect(@post).to be_valid
@@ -11,6 +11,7 @@ RSpec.describe Post, type: :model do
     it "cannot be created without a date and rationale" do
       @post.date = nil
       @post.rationale = nil
+      @post.user_id = nil
       expect(@post).to_not be_valid
     end
   end
