9f058fe454aa207b54adb184714c6d8e0620cf29
Hugo Amorim <hugo@startupdesignninja.com>
2018-04-26 13:51:13 -0300

    style sign in form

--- Gemfile
+++ Gemfile
@@ -17,7 +17,8 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
-
+gem "gritter", "1.2.0"
+gem 'webpacker', '~> 3.4'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/javascript/components/growl.js
@@ -0,0 +1,4 @@
+$.growl({ title: "Growl", message: "The kitten is awake!", url: "/kittens" });
+$.growl.error({ message: "The kitten is attacking!" });
+$.growl.notice({ message: "The kitten is cute!" });
+$.growl.warning({ message: "The kitten is ugly!" });
