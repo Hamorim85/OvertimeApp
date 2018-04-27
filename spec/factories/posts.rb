fbc1a2278a90c67d81b9dee757501d531857ca80
Hugo Amorim <hugo@startupdesignninja.com>
2018-04-27 09:31:00 -0300

    Implemented Enum component to track workflow Statuses

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,11 +2,13 @@ FactoryBot.define do
   factory :post do
     date Date.today
     rationale "some rationale"
+    status
     user
   end
   factory :second_post,  class: "Post" do
     date Date.yesterday
     rationale "some aditional rationale"
+    status
     user
   end
 end
