baa41b6fbfe33008b0d6e2baf151e78d78605d7f
Hugo Amorim <hugo@startupdesignninja.com>
2018-05-08 10:11:08 -0300

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,6 +16,13 @@ AdminUser.create(email: "admin@test.com",
 
 puts "1 Admin User created"
 
+
+AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today - 20.days))
+
+puts "3 Audit Logs have been created"
+
 100.times do |post|
   Post.create(date: Date.today, rationale: "#{post} rationale content Lorem ipsum
     dolor sit amet, consectetur adipiscing elit. Nam eu ex enim. Proin sed justo
@@ -27,10 +34,4 @@ end
 puts "100 Posts have been created"
 
 
-AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today - 13.days))
-AuditLog.create!(user_id: @user.id, status: 0, start_date:(Date.today - 20.days))
-
-
-puts "3 Audit Logs have been created"
 
